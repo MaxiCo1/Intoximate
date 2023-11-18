438bd7b8782b166ec99ba979286c68b78a0c65df
MaxiCo1 <coletti.maxi@gmail.com>
2023-11-17 21:40:18 -0300

    search results page working + randomButton created + random button call to api with bug but almost working

--- app/SearchResults.jsx
+++ app/SearchResults.jsx
@@ -1,14 +1,23 @@
 import React, { useState, useEffect } from "react";
-import { View, Text } from "react-native";
-import CardDrinks from "./componentes/CardDrinks";
-
+import { View, Text, StyleSheet } from "react-native";
+import { SearchBar } from "@rneui/themed";
 import { useLocalSearchParams } from "expo-router";
+
+import CardDrinks from "./componentes/CardDrinks";
+import RandomButton from "./componentes/RandomButton";
 
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 
 const SearchResults = () => {
   const [SearchData, setSearchData] = useState([]);
+  const [NewSearchData, setNewSearchData] = useState([]);
+
   const { palabra } = useLocalSearchParams();
+  const [search, setSearch] = useState("");
+
+  const updateSearch = (search) => {
+    setSearch(search);
+  };
 
   const getData = async () => {
     try {
@@ -25,16 +34,89 @@ const SearchResults = () => {
     getData();
   }, []);
 
-  console.log(SearchData);
+  const getNewData = async () => {
+    try {
+      const response = await fetch(url + search);
+      const NewSearchData = await response.json();
+
+      setNewSearchData(NewSearchData.drinks);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
-    <View>
-      <Text>Resultados de búsqueda</Text>
-      {SearchData.map((drink, index) => (
-        <CardDrinks key={index} drink={drink} />
-      ))}
+    <View
+      style={{
+        backgroundColor: "#212121",
+        height: "100%",
+        width: "100%",
+      }}
+    >
+      <View
+        style={{
+          backgroundColor: "#3B3059",
+          height: "95%",
+          marginTop: "10%",
+          borderTopEndRadius: 50,
+          borderTopStartRadius: 50,
+          alignItems: "center",
+        }}
+      >
+        <Text style={styles.title}>
+          {search != ""
+            ? "Resultados de " + search
+            : "Resultados de " + palabra}
+        </Text>
+        <SearchBar
+          platform="ios"
+          placeholder="Buscar"
+          containerStyle={{
+            width: "90%",
+            backgroundColor: "transparent",
+          }}
+          inputContainerStyle={{
+            borderRadius: 22,
+            backgroundColor: "#494163",
+            color: "#EBEBF5",
+          }}
+          onChangeText={updateSearch}
+          onSubmitEditing={getNewData}
+          value={search}
+        />
+        <RandomButton />
+        <View style={styles.containerDrinks}>
+          {
+            /*SearchData.map((drink, index) => (
+            <CardDrinks key={index} drink={drink} />
+          ))*/
+            search != ""
+              ? NewSearchData.map((drink, index) => (
+                  <CardDrinks key={index} drink={drink} />
+                ))
+              : SearchData.map((drink, index) => (
+                  <CardDrinks key={index} drink={drink} />
+                ))
+          }
+        </View>
+      </View>
     </View>
   );
 };
+const styles = StyleSheet.create({
+  containerDrinks: {
+    margin: "5%",
+    height: "50%",
+    justifyContent: "space-around",
+    flexWrap: "wrap",
+    flexDirection: "row",
+  },
+  containerRecomended: { height: "40%" },
+  title: {
+    color: "#FBFCF8",
+    fontSize: 24,
+    marginTop: "5%",
+  },
+});
 
 export default SearchResults;
