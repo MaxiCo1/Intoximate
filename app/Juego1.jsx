7d4667ba76a177347e808a78bb1aaa853a831ed6
cdejtiar <cdejtiar14@gmail.com>
2023-11-21 19:33:17 -0300

    trying to make it look better

--- app/Juego1.jsx
+++ app/Juego1.jsx
@@ -1,4 +1,11 @@
-import { View, Text, StyleSheet, Button } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  ScrollView,
+  SafeAreaView,
+} from "react-native";
 import React from "react";
 import { useState } from "react";
 import { LinearGradient } from "expo-linear-gradient";
@@ -6,7 +13,9 @@ import games from "./games.json";
 import BackButton from "./componentes/BackButton";
 
 const Juego1 = () => {
-  const [preguntaActual, setPreguntaActual] = useState("Clickeá YO NUNCA NUNCA para jugar");
+  const [preguntaActual, setPreguntaActual] = useState(
+    "Clickeá YO NUNCA NUNCA para jugar"
+  );
 
   const obtenerPreguntaAleatoria = () => {
     const preguntasYoNunca = games.yo_nunca_nunca;
@@ -15,28 +24,38 @@ const Juego1 = () => {
     setPreguntaActual(preguntaAleatoria);
   };
   return (
-    <View style={{ backgroundColor: "#212121", width: "100%", height: "100%" }}>
+    <View
+      style={{
+        backgroundColor: "#212121",
+        width: "100%",
+        height: "100%",
+        flex: 1
+      }}
+    >
       <BackButton />
-      <View style={styles.containerViolet}>
-        <Text style={styles.tituloSeccion}>Yo Nunca Nunca</Text>
-        <View style={styles.imgContainer}>
-          <LinearGradient
-            colors={["#714FA1", "#79D9D4", "#EC5B75", "#79D9D4", "#714FA1"]}
-            start={{ x: 0, y: 0 }}
-            end={{ x: 1, y: 1 }}
-            locations={[0, 0.25, 0.5, 0.75, 1]} // Ajusta las ubicaciones para lograr el efecto deseado
-            style={[styles.linearGradient, styles.glow]}
-          >
-            <View style={[styles.innerContainer]}>
-              <Text style={styles.texto}>{preguntaActual}</Text>
-            </View>
-          </LinearGradient>
-        </View>
-      </View>
-      <Button style={styles.boton}
-        title="YO NUNCA NUNCA"
-        onPress={obtenerPreguntaAleatoria}
-      />
+      <ScrollView style={styles.container}>
+        <SafeAreaView>
+          <Text style={styles.tituloSeccion}>Yo Nunca Nunca</Text>
+          <View style={styles.imgContainer}>
+            <LinearGradient
+              colors={["#714FA1", "#79D9D4", "#EC5B75", "#79D9D4", "#714FA1"]}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 1 }}
+              locations={[0, 0.25, 0.5, 0.75, 1]} // Ajusta las ubicaciones para lograr el efecto deseado
+              style={[styles.linearGradient, styles.glow]}
+            >
+              <View style={[styles.innerContainer]}>
+                <Text style={styles.texto}>{preguntaActual}</Text>
+              </View>
+            </LinearGradient>
+          </View>
+          <Button
+            style={styles.boton}
+            title="YO NUNCA NUNCA"
+            onPress={obtenerPreguntaAleatoria}
+          />
+        </SafeAreaView>
+      </ScrollView>
     </View>
   );
 };
@@ -81,20 +100,14 @@ const styles = StyleSheet.create({
     borderRadius: 25,
   },
   container: {
-    width: "90%",
-    alignItems: "left",
-  },
-  containerViolet: {
     display: "flex",
     flexDirection: "column",
-    justifyContent: "space-around",
+    height: "100%",
+    width: "100%",
+    flex:1,
     backgroundColor: "#3B3059",
-    height: "95%",
-    marginTop: "10%",
     borderTopEndRadius: 50,
     borderTopStartRadius: 50,
-    alignItems: "center",
-    position: "relative"
   }
 });
 
